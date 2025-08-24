f587189da27ecc2ad871369f8bb19d5ea7859113
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-24 18:40:20 +0545

    Create function to display register's binary status
    - The gpip_pin_status function is supposed to show the status of each
      pin. That way we can sort of know which pin was turned on or off,
      virtually of course.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
+void gpio_pin_status(uint8_t port) {
+  char binary[8];
+  for (uint8_t i = 0; i < 8; i++) {
+    binary[(sizeof binary - sizeof binary[i]) - i] = (port >> i) & 0x01;
+    if (i == 7) {
+      for (i = 0; i < sizeof binary; i++) {
+        printf("%d", binary[i]);
+      }
+      break;
+    }
+  }
+  printf("\n");
+}
+
 int main() {
   uint8_t PORTA = 0x00;
-  printf("%d\n", PORTA);
+  gpio_pin_status(PORTA);
 
   return 0;
 }
