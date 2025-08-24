8546e1a6347bea024fc55c1edb4aac458c22e178
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-24 14:10:52 +0545

    Initialise the main, makefile and gitignore files

---
+++ Makefile
@@ -0,0 +1,5 @@
+compile:
+	gcc src/*.c -o bin/GPIO-simulator
+
+run:compile
+	bin/GPIO-simulator
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main() {
+  uint8_t PORTA = 0x00;
+  printf("%d\n", PORTA);
+
+  return 0;
+}
