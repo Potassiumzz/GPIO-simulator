eebace5e559250564e847d420e99b1de3bb7e923
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-24 19:44:18 +0545

    Create a format file
    - The reason for this was the column width by the default clang format
      was too narrow, and even the small comment for code or a little longer
      functions would make it go to the multi line making it slightly harder
      to read and wierd looking as well, and it was pissing me off.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ void gpio_pin_status(uint8_t port) {
   char binary[8];
   for (uint8_t i = 0; i < 8; i++) {
     binary[(sizeof binary - sizeof binary[i]) - i] = (port >> i) & 0x01;
+    // Check if all the bits were stored in the binary array, and print the result.
     if (i == 7) {
       for (i = 0; i < sizeof binary; i++) {
         printf("%d", binary[i]);
