9c3607e2e54f1dc736438f327725e46d5d73c16a
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-29 13:42:38 +0545

    Implement a basic traffic light with FSM
    - Simulate a FSM (Finite State Machine) traffic light system using GPIO
      pins.
      The implementation of RED -> RED + YELLOW -> GREEN -> GREEN + YELLOW
      -> RED and so on was designed for this sytem with a built in sleep
      function.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
+
+enum TRAFFIC_LIGHTS { RED = 1, YELLOW = 2, GREEN = 4 };
 
 /*
  * Display the current status of the GPIO pins (8 bits) as individual bits.
@@ -44,14 +47,45 @@ void gpio_pin_toggle(uint8_t *port, uint8_t pin) { *port ^= (1 << pin); }
 int main() {
   uint8_t PORTA = 0x00;
   gpio_pin_status(PORTA); // prints 00000000
+  enum TRAFFIC_LIGHTS light = RED;
 
-  gpio_pin_set(&PORTA, 4);
-  gpio_pin_set(&PORTA, 7);
-  gpio_pin_status(PORTA); // prints 10010000
-  gpio_pin_clear(&PORTA, 7);
-  gpio_pin_status(PORTA); // prints 00010000
-  gpio_pin_toggle(&PORTA, 4);
-  gpio_pin_status(PORTA); // prints 00000000
+  while (1) {
+    switch (light) {
+    case RED:
+      printf("RED ");
+      gpio_pin_set(&PORTA, 0);
+      gpio_pin_clear(&PORTA, 1); // turn off the yellow light
+      gpio_pin_status(PORTA);
+      sleep(2);
+      light = YELLOW;
+      break;
+    case YELLOW:
+      printf("YELLOW ");
+      gpio_pin_set(&PORTA, 1);
+      gpio_pin_status(PORTA);
+      if (PORTA == 0x06) {
+        printf("YELLOW + GREEN ");
+        light = RED;
+        gpio_pin_clear(&PORTA, 2); // turn off the green light
+      } else if (PORTA == 0x03) {
+        printf("YELLOW + RED ");
+        gpio_pin_status(PORTA);
+        light = GREEN;
+        gpio_pin_clear(&PORTA, 0); // turn off the red light
+      }
+      gpio_pin_status(PORTA);
+      sleep(2);
+      break;
+    case GREEN:
+      printf("GREEN ");
+      gpio_pin_clear(&PORTA, 1); // turn off the yellow light
+      gpio_pin_set(&PORTA, 2);
+      gpio_pin_status(PORTA);
+      sleep(2);
+      light = YELLOW;
+      break;
+    }
+  }
 
   return 0;
 }
