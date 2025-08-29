8a729437d1a51727c7b3cdf938835f6f10199801
Potassiumxx <brainiacgamer444@gmail.com>
2025-08-29 20:46:30 +0545

    Add more traffic light states in enum
    - The logic for YELLOW light case was getting too complex. The simple
      soultion would be to just add more traffic light states instead of
      using if and else inside the YELLOW case (switch statement).
    
      So now we do not play the if and else game and just jump from one case
      to another.

--- src/traffic_light.c
+++ src/traffic_light.c
@@ -1,13 +1,14 @@
 #include <gpio.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <traffic_light.h>
 #include <unistd.h>
 
-enum TRAFFIC_LIGHTS { RED = 1, YELLOW = 2, GREEN = 4 };
+enum TRAFFIC_LIGHTS { RED = 1, YELLOW = 2, RED_YELLOW = 3, GREEN = 4, GREEN_YELLOW = 6 };
 
 void traffic_light_cycle(uint8_t *port) {
-
   enum TRAFFIC_LIGHTS light = RED;
+  // uint8_t loop_yellow_light = (*port >> 5) & 0x01;
 
   while (1) {
     switch (light) {
@@ -17,26 +18,19 @@ void traffic_light_cycle(uint8_t *port) {
       gpio_pin_clear(port, 1); // turn off the yellow light
       gpio_pin_status(*port);
       sleep(2);
-      light = YELLOW;
+      light = RED_YELLOW;
       break;
     case YELLOW:
       printf("YELLOW ");
       gpio_pin_set(port, 1);
       gpio_pin_status(*port);
-      if (*port == (YELLOW | GREEN)) {
-        printf("YELLOW + GREEN ");
-        gpio_pin_status(*port);
-        sleep(2);
-        light = RED;
-        gpio_pin_clear(port, 2); // turn off the green light
-      } else if (*port == (YELLOW | RED)) {
-        printf("YELLOW + RED ");
-        gpio_pin_status(*port);
-        sleep(2);
-        light = GREEN;
-        gpio_pin_clear(port, 0); // turn off the red light
-      }
+      break;
+    case RED_YELLOW:
+      printf("YELLOW + RED ");
       gpio_pin_status(*port);
+      sleep(2);
+      light = GREEN;
+      gpio_pin_clear(port, 0); // turn off the red light
       break;
     case GREEN:
       printf("GREEN ");
@@ -44,7 +38,14 @@ void traffic_light_cycle(uint8_t *port) {
       gpio_pin_set(port, 2);
       gpio_pin_status(*port);
       sleep(2);
-      light = YELLOW;
+      light = GREEN_YELLOW;
+      break;
+    case GREEN_YELLOW:
+      printf("YELLOW + GREEN ");
+      gpio_pin_status(*port);
+      sleep(2);
+      light = RED;
+      gpio_pin_clear(port, 2); // turn off the green light
       break;
     }
   }
